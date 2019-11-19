d6fe5aa10932f0b583df5477abf127b0f4885a04
alumno <alumno@aula1-02>
2019-11-19 14:35:24 +0100

    Terminado comprobar si CCC es válido

--- cuentaBancaria/src/cuentaBancaria/AplicaciónCuentaBancaria.java
+++ cuentaBancaria/src/cuentaBancaria/AplicaciónCuentaBancaria.java
@@ -8,49 +8,68 @@ public class AplicaciónCuentaBancaria {
 		Scanner leer = new Scanner(System.in);
 		String nombre = "";
 		String CCC = "";
+		String entidad, oficina, numeroCuenta, digitosControl;
 		System.out.println("Pon el nombre del titular");
 		nombre = leer.nextLine();
 		int opcion = 0;
 		Menú menu = new Menú();
-		CuentaBancaria cb1 = new CuentaBancaria(nombre, CCC);
+		CuentaBancaria cb1; 
 		do {
-			opcion = menu.pintaMenu();
-			switch (opcion) {
-			case 1:
-				System.out.println(cb1.getCCC());
-				break;
-			case 2:
-				System.out.println(cb1.getNombre());
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-			case 5:
-				
-				break;
-			case 6:
-				
-				break;
-			case 7:
-				cb1.ingresarSaldo();
-				break;
-			case 8:
-				cb1.retirarDinero();
-				break;
-			case 9:
-				System.out.println(cb1.getSaldo());
-				break;
-			case 10:
-				System.out.println("Saliendo");
-				break;
-			default:
-				System.out.println("Opción no válida");
-				break;
-			}
-		}while(opcion != 10);
+			System.out.println("Pon la entidad del codigo de cuenta");
+			entidad = leer.nextLine();
+			System.out.println("Pon la oficina del codigo de cuenta");
+			oficina = leer.nextLine();
+			System.out.println("Pon los dígitos de control del codigo de cuenta");
+			digitosControl = leer.nextLine();
+			System.out.println("Pon el numero de cuenta del codigo de cuenta");
+			numeroCuenta = leer.nextLine();
+			CCC = entidad + "-" + oficina + "-" + digitosControl + "-" + numeroCuenta;
+			cb1 = new CuentaBancaria(nombre, CCC);
+		}while(cb1.comprobarCCC()==false);
+		
+		if(cb1.comprobarCCC()) {
+			do {
+				opcion = menu.pintaMenu();
+				switch (opcion) {
+				case 1:
+					System.out.println(cb1.getCCC());
+					break;
+				case 2:
+					System.out.println(cb1.getNombre());
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+				case 5:
+					
+					break;
+				case 6:
+					
+					break;
+				case 7:
+					System.out.println("Pon el dinero que quieres ingresar");
+					double dineroIngresado = leer.nextDouble();
+					cb1.ingresarSaldo(dineroIngresado);
+					break;
+				case 8:
+					System.out.println("Pon la cantidad de dinero retirado");
+					double dineroRetirado = leer.nextDouble();
+					cb1.retirarDinero(dineroRetirado);
+					break;
+				case 9:
+					System.out.println(cb1.getSaldo());
+					break;
+				case 10:
+					System.out.println("Saliendo");
+					break;
+				default:
+					System.out.println("Opción no válida");
+					break;
+				}
+			}while(opcion != 10);
+		}
 	}
-
 }
--- cuentaBancaria/src/cuentaBancaria/CuentaBancaria.java
+++ cuentaBancaria/src/cuentaBancaria/CuentaBancaria.java
@@ -5,62 +5,121 @@ import java.util.Scanner;
 public class CuentaBancaria {
 
 	private String nombre;
-	private String CCC;
+	private String ccc;
 	private double saldo = 0;
-
+	
+	//Aquí se crea la clase CuentaBancaria
 	public CuentaBancaria(String nombre, String CCC) {
 		this.nombre = nombre;
-		this.CCC = CCC;
+		this.ccc = CCC;
 	}
-
+	//Este método muestra el nombre del titular del objeto
 	public String getNombre() {
 		return nombre;
 	}
-
+	//Este método almacena el nombre del titular del objeto
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	//Este método muestra el CCC del objeto
 	public String getCCC() {
-		return CCC;
+		return ccc;
 	}
-
+	//Este método almacena el CCC del objeto
 	public void setCCC(String CCC) {
-		this.CCC = CCC;
+		this.ccc = CCC;
 	}
-
+	//Este método muestra el saldo del objeto
 	public double getSaldo() {
 		return this.saldo;
 	}
-
+	//Este método almacena el saldo del objeto
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+	//Este método permite realizar un ingreso
+	public void ingresarSaldo(double dineroIngresado) {
 
-	public void ingresarSaldo() {
-		Scanner leer = new Scanner (System.in);
-		double dineroIngresado;
-		System.out.println("Pon la cantidad de dinero ingresado");
-		dineroIngresado = leer.nextInt();
+	
 		if(dineroIngresado<0) {
 			System.out.println("No puedes poner un número negativo");
 		}else {
-			saldo = saldo + dineroIngresado;
+			saldo += dineroIngresado;
 		}
 	}
-	public void retirarDinero() {
-		Scanner leer = new Scanner (System.in);
-		double dineroRetirado;
-		System.out.println("Pon la cantidad de dinero retirado");
-		dineroRetirado = leer.nextInt();
+	//Este método permite retirar efectivo del saldo
+	public void retirarDinero(double dineroRetirado) {
 		if(dineroRetirado<0) {
 			System.out.println("No puedes poner un número negativo");
 		}else {
 			if(saldo<dineroRetirado) {
 				System.out.println("No tienes suficiente saldo para retiralo");
 			}else {
-				saldo = saldo - dineroRetirado;
+				saldo -= dineroRetirado;
 			}
+		}
+	}
+	//Este método permite saber si el CCC es válido o no
+	public boolean comprobarCCC() {
+		int[] vCantidadDigitos = new int[4];
+		int[] digitoControlCifra = {1, 2, 4, 8, 5, 10, 9, 7, 3, 6};
+		int digito1, digito2; 
+		int suma1 = 0;
+		int suma2 = 0;
+		int j = 0;
+		for(int i = 0; i<ccc.length(); i++) {
+			if(ccc.substring(i, i+1).equalsIgnoreCase("-")) {
+				j++;
+			}else {
+				vCantidadDigitos[j]++;
+			}
+		}
+		if((vCantidadDigitos[0] == 4) && (vCantidadDigitos[1] == 4) && (vCantidadDigitos[2] == 2) && (vCantidadDigitos[3] == 10)) {
+			suma1 = (Integer.valueOf(ccc.substring(0, 1))) * 4; 
+			suma1 += (Integer.valueOf(ccc.substring(1, 2))) * 8;
+			suma1 += (Integer.valueOf(ccc.substring(2, 3))) * 5;
+			suma1 += (Integer.valueOf(ccc.substring(3, 4))) * 10;
+			suma1 += (Integer.valueOf(ccc.substring(5, 6))) * 9; 
+			suma1 += (Integer.valueOf(ccc.substring(6, 7))) * 7;
+			suma1 += (Integer.valueOf(ccc.substring(7, 8))) * 3; 
+			suma1 += (Integer.valueOf(ccc.substring(8, 9))) * 6;
+			
+			if((11 - (suma1%11)) == 10) {
+				digito1 = 1;
+			}else {
+				if((11-(suma1%11)) == 11) {
+					digito1 = 0;
+				}else {
+					digito1 = 11-(suma1%11);
+				}
+			}
+			suma2 = (Integer.valueOf(ccc.substring(13, 14))) * 1;
+			suma2 += (Integer.valueOf(ccc.substring(14, 15))) * 2;
+			suma2 += (Integer.valueOf(ccc.substring(15, 16))) * 4;
+			suma2 += (Integer.valueOf(ccc.substring(16, 17))) * 8;
+			suma2 += (Integer.valueOf(ccc.substring(17, 18))) * 5;
+			suma2 += (Integer.valueOf(ccc.substring(18, 19))) * 10;
+			suma2 += (Integer.valueOf(ccc.substring(19, 20))) * 9;
+			suma2 += (Integer.valueOf(ccc.substring(20, 21))) * 7;
+			suma2 += (Integer.valueOf(ccc.substring(21, 22))) * 3;
+			suma2 += (Integer.valueOf(ccc.substring(22, 23))) * 6;
+			
+			if((11 - (suma2%11)) == 10) {
+				digito2 = 1;
+			}else {
+				if((11-(suma2%11)) == 11) {
+					digito2 = 0;
+				}else {
+					digito2 = 11-(suma2%11);
+				}
+			}
+			if(Integer.valueOf(ccc.substring(10, 11)) == digito1 && (Integer.valueOf(ccc.substring(11, 12)) == digito2)) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
 		}
 	}
 }
